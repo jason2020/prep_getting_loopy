bd53a7f27748bdde868cbc2ae6201c9f8ba9a212
jason2020 <jtay20@punahou.edu>
2016-06-24 14:16:41 -1000

    Finished challenge 1

--- loops.js
+++ loops.js
@@ -1,1 +1,9 @@
 // Do your work here
+
+var array = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
+var length = array.length;
+
+for (var i = 0; i < length; i++) {
+	console.log("Value of i is:" + i);
+	console.log("Value at Index is: " + array[i]);
+}
