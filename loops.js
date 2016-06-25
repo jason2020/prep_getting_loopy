fb5589d1d507d471739b0d2aab0461ac77cde9c7
jason2020 <jtay20@punahou.edu>
2016-06-24 14:22:32 -1000

    Finished challenge 2

--- loops.js
+++ loops.js
@@ -1,5 +1,6 @@
 // Do your work here
 
+/* Challenge 1
 var array = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
 var length = array.length;
 
@@ -7,3 +8,17 @@ for (var i = 0; i < length; i++) {
 	console.log("Value of i is:" + i);
 	console.log("Value at Index is: " + array[i]);
 }
+*/
+
+
+/* Challenge 2
+var evenNumberArray = [];
+
+for (var i = 0; i < 50; i++) {
+	evenNumberArray.push(i * 2);
+}
+
+console.log(evenNumberArray);
+*/
+
+
