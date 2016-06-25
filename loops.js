9a9b5fdcf28dce7c40b5927f82d7536c4811c69c
jason2020 <jtay20@punahou.edu>
2016-06-24 14:50:43 -1000

    Added functions and challenge 2, I'm now on challenge 6

--- loops.js
+++ loops.js
@@ -1,42 +1,74 @@
 // Do your work here
 
 /* Challenge 1
-var array = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
-var length = array.length;
+var presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
 
-for (var i = 0; i < length; i++) {
-	console.log("Value of i is:" + i);
-	console.log("Value at Index is: " + array[i]);
-}
-*/
+function loop(array) {
+	var length = array.length;
 
-
-/* Challenge 2
-var evenNumberArray = [];
-
-for (var i = 0; i < 50; i++) {
-	evenNumberArray.push(i * 2);
-}
-
-console.log(evenNumberArray);
-*/
-
-/*
-var oopsArray = ['turn' , , 'down' , , 'for' , , 'what'];
-var length = oopsArray.length;
-
-for (var i = 1; i < length; i++) {
-	if (i % 2 !== 0) {
-		oopsArray.splice(i, 1, "nope");
+	for (var i = 0; i < length; i++) {
+		console.log("Value of i is:" + i);
+		console.log("Value at Index is: " + array[i]);
 	}
 }
 
-console.log(oopsArray);
+loop(presidents);
 */
 
-var oopsArray = [ 'turn' , 'nope' , 'down' , 'nope' , 'for' , 'nope' , 'what' ];
-var length = oopsArray.length;
+/* Challenge 2
+var stringOfNumbers = '';
 
-for (var i = length - 1; i >= 0; i--) {
-	console.log(oopsArray[i]);
+function loop(str) {
+	for (var i = 10; i < 21; i++) {
+		str += i;
+	}
+	console.log(str)
 }
+
+loop(stringOfNumbers);
+*/
+
+
+/* Challenge 3
+var evenNumberArray = [];
+
+function loop(array) {
+	for (var i = 0; i < 51; i++) {
+		array.push(i * 2);
+	}
+	console.log(array);
+}
+
+loop(evenNumberArray);
+*/
+
+/* Challenge 4
+var oopsArray = ['turn' , , 'down' , , 'for' , , 'what'];
+
+
+function loop(array) {
+	var length = array.length;
+	for (var i = 1; i < length; i++) {
+		if (i % 2 !== 0) {
+			array.splice(i, 1, "nope");
+		}
+	}
+	console.log(array);
+}
+
+loop(oopsArray);
+*/
+
+/* Challenge 5
+var oopsArray = [ 'turn' , 'nope' , 'down' , 'nope' , 'for' , 'nope' , 'what' ];
+
+function loop(array) {
+	var length = array.length;
+
+	for (var i = length - 1; i >= 0; i--) {
+		console.log(array[i]);
+	}
+}
+
+loop(oopsArray)
+*/
